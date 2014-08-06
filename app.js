8dde048f8cdbc3790fee85d7e9fe9404fc519568
Manske <Manske@Nates-MacBook-Pro-3.local>
2014-08-05 20:45:42 -0500

    Updated FizzBuzz

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
-	$('#button').click(function(){
-		for (var i = 0; i < 101; i++) {
-			if (((i % 3) == 0) && ((i % 5) != 0)) {
-				$('#display').append('<div class="displayed"> fizz </div>');
-			}
-			else if (((i % 5) == 0) && ((i % 3) != 0)) {
-				$('#display').append('<div class="displayed"> buzz </div>');
-			}
-			else if (((i % 5) == 0) && ((i % 3) == 0)) {
-				$('#display').append('<div class="displayed"> fizz buzz </div>');
-			}
-			else {
-				$('#display').append('<div class="displayed">' + i + '</div>');
-			}
+	var numCount = +prompt("Enter Number");
+		while (numCount % 1 != 0) {
+			var numCount = +prompt("Enter Whole Number");
 		}
-	});
+	var fizzBuzz = function() {
+		$('#button').click(function(){
+			$('.displayed').fadeOut('fast');
+			for (var i = 1; i < (numCount + 1); i++) {
+				if (((i % 3) == 0) && ((i % 5) != 0)) {
+					$('#display').append('<div class="displayed"> fizz </div>');
+				}
+				else if (((i % 5) == 0) && ((i % 3) != 0)) {
+					$('#display').append('<div class="displayed"> buzz </div>');
+				}
+				else if (((i % 5) == 0) && ((i % 3) == 0)) {
+					$('#display').append('<div class="displayed"> fizz buzz </div>');
+				}
+				else {
+					$('#display').append('<div class="displayed">' + i + '</div>');
+				}
+			}
+		});
+	};
+	fizzBuzz();	
 });
